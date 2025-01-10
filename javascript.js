3554e929d7d29bbd8cdf029b9f6a1698b40736cf
Alex Kara <190240100+akarathanassis@users.noreply.github.com>
2025-01-10 23:33:58 +1100

    Add title and changes to createGrid
    createGrid to be fixed in next commit

--- javascript.js
+++ javascript.js
@@ -1,10 +1,20 @@
 let containerHeight = 500; 
 let containerWidth = 500; 
 let dimension = Number(prompt("Enter number of squares per row!")); 
+const container = document.querySelector(".container");
 
 function createGrid(dimension) { 
-    // create the desired square div and set correct height and width 
-    const square = document.createElement("div"); 
+    // create the desired square div and set correct height and width and border
+    const square = document.createElement("span"); 
+    square.setAttribute("style", 
+                        `height: ${500/dimension}; 
+                        width: ${500/dimension}; 
+                        border: solid black;`)
 
     // now create a grid of dimension x dimension square divs 
+    for (let i=0; i<dimension*dimension; i++) { 
+        container.appendChild(square); 
+    }
 }
+
+createGrid(dimension); 

a06ec034286e82f1e6f941915f46e1dce7d5654d
Alex Kara <190240100+akarathanassis@users.noreply.github.com>
2025-01-10 23:22:18 +1100

    Defined container div dimensions + createGrid func
    Finish createGrid func to show complete grid on pg

---
+++ javascript.js
@@ -0,0 +1,10 @@
+let containerHeight = 500; 
+let containerWidth = 500; 
+let dimension = Number(prompt("Enter number of squares per row!")); 
+
+function createGrid(dimension) { 
+    // create the desired square div and set correct height and width 
+    const square = document.createElement("div"); 
+
+    // now create a grid of dimension x dimension square divs 
+}

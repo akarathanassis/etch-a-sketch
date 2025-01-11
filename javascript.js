8f0282effd86f0b4a2c3d6095c7dbcd6b1ba72e6
Alex Kara <190240100+akarathanassis@users.noreply.github.com>
2025-01-11 11:23:50 +1100

    Finish createGrid function
    Webpage now creates desired grid

--- javascript.js
+++ javascript.js
@@ -4,15 +4,15 @@ let dimension = Number(prompt("Enter number of squares per row!"));
 const container = document.querySelector(".container");
 
 function createGrid(dimension) { 
-    // create the desired square div and set correct height and width and border
-    const square = document.createElement("span"); 
-    square.setAttribute("style", 
-                        `height: ${500/dimension}; 
-                        width: ${500/dimension}; 
-                        border: solid black;`)
-
     // now create a grid of dimension x dimension square divs 
     for (let i=0; i<dimension*dimension; i++) { 
+        // create the desired square div and set correct height and width and border
+        const square = document.createElement("div"); 
+        square.setAttribute("style", `height: ${500/dimension}px; 
+                            width: ${500/dimension}px; 
+                            border: solid black;`)
+                            
+        square.classList.add("cell"); 
         container.appendChild(square); 
     }
 }
